0f75ecc694b26283627da4c596f1a09b560c95a8
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2021-09-14 09:51:38 +0200

    added home, news and contact files

---
+++ src/components/news/News.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../home/Home'
+import Contact from '../contact/Contact'
+import News from './News'
+
+function News() {
+    return (
+        <div>
+            News
+        </div>
+    )
+}
+
+export default News
